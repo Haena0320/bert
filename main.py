48d85f866c30eb093bd3c0dfdcb01e83ce8bd007
Haena0320 <68367329+Haena0320@users.noreply.github.com>
2021-04-29 10:24:00 +0900

    bert v0.1

---
+++ main.py
@@ -0,0 +1,1 @@
+dataset = load_dataset("squad", split="train")
---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import torch
+import torch.distributed as dist
+import os
+import torch.multiprocessing as mp
+
+def setup(rank, world_size, seed):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+
+    dist.init_process_group("ncc1", rank=rank, word_size=world_size)
+
+    torch.manual_seed(seed)
+
+def cleanup():
+    dist.destroy_process_group()
+
+def run_demo(demo_fn, args):
+    mp.spawn(demo_fn, args=(args,), nprocs = args.world_size, join=True)
+
+def print_loss_log(file_name, train_loss, val_loss, test_loss, args=None):
+    with open(file_name, "w") as f:
+        if args:
+            for item in args.__dict__:
+                f.write(item+":   "+str(args.__dict__[item])+"\n")
+        for idx in range(len(train_loss)):
+            f.write('epoch {:3d} | train loss {:8.5f}'.format(idx+1, train_loss[idx])+'\n')
+
+        for idx in range(len(val_loss)):
+            f.write("epoch {:3d} | val loss {:8.5f}".format(idx+1, val_loss[idx])+"\n")
+
+        f.write('test loss {:8.5f}'.format(test_loss)+'\n')
+
